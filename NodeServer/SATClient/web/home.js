72f8b94fa6f6ae3dbff36ddf6a93240a47b42c50
dariki1 <james_atkins97@hotmail.com>
2017-11-20 10:23:25 +1100

    Basic Client-Server messaging

---
+++ NodeServer/SATClient/web/home.js
@@ -0,0 +1,8 @@
+﻿(function test() {
+    console.log("DOING THE THINGS");
+    console.log("DOING THE THINGS");
+    let socket = io.connect("http://127.0.0.10");
+    socket.on('connect', function () {
+        socket.emit('potato', "I am aeroplane jelly");
+    });
+})();
--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,54 +1,21 @@
-// Dependancies
-var express = require('express');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var app = express();    // Start server
-var router = express.Router();  // Create router instance
-app.use(bodyParser.urlencoded({ extended: false }));    // Used to get body information from webpages
+﻿let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+let port = 80;
 
-//try { app.use(express.static(path.join(__dirname, 'SATClient/public'))); } catch (e) { console.log(e); }// Gets css and javascript files
-
-// Listen on this port
-var port = process.env.PORT || 80;
-var server = app.listen(port);
-console.log("Server running at http://localhost:%d", port);
-
-// Routes
-getRoute('/', 'home.html');
-
-function getRoute(route, path) {
-	router.get(route, function (req, res, next) {
-		res.sendFile(path, { root: __dirname + '/SATClient/web/' });
-	});
-}
-
-// Catches 404
-router.use(function (req, res, next) {
-	res.sendFile('SATClient/web/404.html', { root: __dirname });
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/SATClient/web'));
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + '/SATClient/web/home.html');
 });
 
-app.use('/', router);
+io.on('connection', function(socket) {
+    console.log('Client has connected; ' + socket.id);
 
+    socket.on('potato', function(data) {
+        console.log("[" + socket.id + "]; " + data);
+    });
+});
 
-//POST
-try {
-	app.post("/submit", function (request, response) {
-
-		response.writeHead(200, { "Content-Type": "text/plain" });
-		//response.write(request.item("i"));
-		response.end("Submitted post");
-
-	});
-}
-catch (ex) { console.log(ex.toString()); }
-
-//GET
-try {
-	app.get("/submit", function (request, response) {
-
-		response.writeHead(200, { "Content-Type": "text/plain" });
-		response.end("Submitted get");
-
-	});
-}
-catch (ex) { console.log(ex.toString()); }
+server.listen(port);  
