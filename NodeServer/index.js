04aa081e23369fb24b3cfd91fd94ec03be13bda2
Kyle Sutton <kyle-09@live.com>
2017-11-20 09:29:13 +1100

    Merged index from kyle branch

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,14 +1,54 @@
-var http = require('http');
+// Dependancies
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();    // Start server
+var router = express.Router();  // Create router instance
+app.use(bodyParser.urlencoded({ extended: false }));    // Used to get body information from webpages
 
-var server = http.createServer(function(request, response) {
+//try { app.use(express.static(path.join(__dirname, 'SATClient/public'))); } catch (e) { console.log(e); }// Gets css and javascript files
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+// Listen on this port
+var port = process.env.PORT || 80;
+var server = app.listen(port);
+console.log("Server running at http://localhost:%d", port);
 
+// Routes
+getRoute('/', 'home.html');
+
+function getRoute(route, path) {
+	router.get(route, function (req, res, next) {
+		res.sendFile(path, { root: __dirname + '/SATClient/web/' });
+	});
+}
+
+// Catches 404
+router.use(function (req, res, next) {
+	res.sendFile('SATClient/web/404.html', { root: __dirname });
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+app.use('/', router);
 
-console.log("Server running at http://localhost:%d", port);
-//TEST
+
+//POST
+try {
+	app.post("/submit", function (request, response) {
+
+		response.writeHead(200, { "Content-Type": "text/plain" });
+		//response.write(request.item("i"));
+		response.end("Submitted post");
+
+	});
+}
+catch (ex) { console.log(ex.toString()); }
+
+//GET
+try {
+	app.get("/submit", function (request, response) {
+
+		response.writeHead(200, { "Content-Type": "text/plain" });
+		response.end("Submitted get");
+
+	});
+}
+catch (ex) { console.log(ex.toString()); }
