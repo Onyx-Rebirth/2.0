828a1d57e2c076958dc7dbb4bbbfd0628133467c
Simon Lee <slee1994@hotmail.com>
2017-11-20 13:09:12 +1100

    Created remaining controllers

---
+++ API/Controllers/SessionsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API;
+
+namespace API.Controllers
+{
+    public class SessionsController : ApiController
+    {
+        private onyx2Entities1 db = new onyx2Entities1();
+
+        // GET: api/Sessions
+        public IQueryable<Session> GetSessions()
+        {
+            return db.Sessions;
+        }
+
+        // GET: api/Sessions/5
+        [ResponseType(typeof(Session))]
+        public async Task<IHttpActionResult> GetSession(int id)
+        {
+            Session session = await db.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(session);
+        }
+
+        // PUT: api/Sessions/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutSession(int id, Session session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != session.SessionId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(session).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Sessions
+        [ResponseType(typeof(Session))]
+        public async Task<IHttpActionResult> PostSession(Session session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Sessions.Add(session);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SessionExists(session.SessionId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = session.SessionId }, session);
+        }
+
+        // DELETE: api/Sessions/5
+        [ResponseType(typeof(Session))]
+        public async Task<IHttpActionResult> DeleteSession(int id)
+        {
+            Session session = await db.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            db.Sessions.Remove(session);
+            await db.SaveChangesAsync();
+
+            return Ok(session);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SessionExists(int id)
+        {
+            return db.Sessions.Count(e => e.SessionId == id) > 0;
+        }
+    }
+}
