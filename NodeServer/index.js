11bf93efd747bcfefc2b2906de7cdf0fd1ccd7a0
dariki1 <james_atkins97@hotmail.com>
2017-11-21 11:45:48 +1100

    Cleaned up index and reorganised some files

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,7 +1,3 @@
-//TODO; FIX PAGE ROUTING
-
-
-
 //Custom module import
 //let t = require('./TestModule.js');
 //Express, to serve the webpages
@@ -14,42 +10,18 @@ let server = require('http').createServer(app);
 let io = require('socket.io')(server);
 //The port the server runs on
 let port = 80;
-let router = express.Router();
 
 //Location of files the client uses
 app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/SATClient'));
-
-router.use(function (req, res) {
-    res.sendFile(__dirname + '/SATClient/web/template.html');
+app.get('/', function(req,res,next) {
+    res.sendFile(__dirname + '/SATClient/web/home.html');
 });
-/*
-function addRoute(dir) {
-    let s = dir.split('/');
-    s = s[s.length - 1];
-    console.log(dir);
-    console.log(s);
-    console.log(dir.substring(0, dir.length - s.length - 1));
-    
-    router.get('/' + dir.substring(0, dir.length - s.length - 1), function (req, res) {
-        res.sendFile(__dirname + '/SATClient/web/template.html');
-    });
-}
-*/
 
 //Gets called when new client connects
 io.on('connection', function (socket) {
     console.log('Client has connected; ' + socket.id);
-
-    //Example listener
-    socket.on('potato', function (data) {
-        console.log("[" + socket.id + "]; " + data);
-        //Broadcasts to all connected users
-        io.emit('cake','a');
-    });
 });
-
-app.use('/',router);
 
 //Hosts the server on port
 server.listen(port);
